8bdf18a5085a80fcc1c6edd100381ec063694b30
Jakub <jmarecek@users.noreply.github.com>
2023-02-26 12:59:49 +0100

    Update parallel2.cpp

--- code_examples/openmp/parallel2.cpp
+++ code_examples/openmp/parallel2.cpp
@@ -5,11 +5,12 @@
 int main() {
 
    omp_set_nested(1);
+   int iam, nt; 
 
 #pragma omp parallel num_threads(2) private(iam,nt)
 {
-        int iam = omp_get_thread_num();
-        int nt =  omp_get_num_threads();
+        iam = omp_get_thread_num();
+        nt =  omp_get_num_threads();
 	std::osyncstream(std::cout) << "L1: " << iam << "/" << nt << std::endl;
 
 #pragma omp parallel num_threads(2) private(iam,nt)
