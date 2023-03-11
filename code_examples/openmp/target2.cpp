4c4b405d38497fd82933d68affa4f360c5e7b8b6
Jakub Marecek <jakub@MacBook-Air.local>
2023-03-11 16:10:28 +0100

    minor edits

--- code_examples/openmp/target2.cpp
+++ code_examples/openmp/target2.cpp
@@ -1,22 +1,15 @@
-// As per https://www.openmp.org/wp-content/uploads/2021-10-20-Webinar-OpenMP-Offload-Programming-Introduction.pdf, best compiled with:
-// clang/LLVM: clang -fopenmp -fopenmp-targets=<target triple>
-// GNU: gcc -fopenmp
-// AMD ROCm: clang -fopenmp -offload-arch=gfx908
-// NVIDIA: nvcc –mp=gpu -gpu=cc80
-// Intel: icx -fiopenmp -fopenmp-targets=spir64
-// IBM XL: xlc –qsmp –qoffload –qtgtarch=sm_70
-
 #include <vector>
 #include "omp.h"
 #include "target2.h"
 
 int main() {
-  float a = 1.0;
-  int sz = 1000;
+  float a = 3.1415;
+  int sz = 1048576;
+  int num_blocks = 4096;
   std::vector<float> vx(sz);
   float* x = vx.data();
   std::vector<float> vy(sz);
   float* y = vy.data();
-  saxpy(a, x, y, sz);    
+  saxpy(a, x, y, sz, num_blocks);    
   return 0;
 }
